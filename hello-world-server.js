f452f89baf646643026166c1a1547ca7da15ab75
yoshikinakamura <yoshiki_nakamura@relationsgroup.co.jp>
2014-05-24 14:46:47 +0900

    list2-2

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,6 +1,12 @@
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<html>');
+  res.write('<head><title></title></head>');
+  res.write('<body>');
+  res.write('<h1>Hello!</h1>');
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
