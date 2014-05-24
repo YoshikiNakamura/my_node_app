d1bbb230fee25bc7c41e471d78128bcf96156ea3
yoshikinakamura <yoshiki_nakamura@relationsgroup.co.jp>
2014-05-24 17:37:33 +0900

    list2-15

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,7 +1,10 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var qs = require('querystring');
+var ejs = require('ejs');
+
+var index = fs.readFileSync('./index.ejs', 'utf8');
+var style = fs.readFileSync('./style.css', 'utf8');
 
 var server = http.createServer();
 server.on('request', doRequest);
@@ -13,41 +16,18 @@ function doRequest(req, res)
 	switch(path.pathname)
 	{
 		case '/':
-			fs.readFile('./index.html', 'UTF-8', doRead);
-			function doRead(err, data)
-			{
-				var result = data.replace(/@@@@@/, '何か書いて。');
-				res.setHeader('Content-Type', 'text/html');
-				res.write(result);
-				res.end();
-			}
+			var tmp = ejs.render(index, {title:"タイトル", msg:"メッセージ"});
+			res.setHeader('Content-Type', 'text/html');
+			res.write(tmp);
+			res.end();
 			break;
-		case '/form':
-			if(req.method == "POST")
-			{
-				var reqBody = '';
-				req.on('data', function(data){reqBody += data;});
-				req.on('end', function()
-				{
-					var form = qs.parse(reqBody);
-					var input1 = form.input1;
-					fs.readFile('./index.html', 'UTF-8', function(err, data)
-					{
-						var result = data.replace(/@@@@@/, "あなたは「"+input1+"」と書きました。");
-						res.setHeader('Content-Type', 'text/html');
-						res.write(result);
-						res.end();
-					});
-				});
-			}
-			else
-			{
-				res.setHeader('Content-Type', 'text/plane');
-				res.end("ERROR! - can't get");
-			}
+		case '/style.css':
+			res.setHeader('Content-Type', 'text/css');
+			res.write(style);
+			res.end();
 			break;
 		default:
-			res.setHeader('Content-Type', 'text/html');
+			res.setHeader('Content-Type', 'text/plain');
 			res.end('ERROR! - NO PAGE');
 			break;
 	}
